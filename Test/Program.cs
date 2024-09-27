21840cf30a993352a167d4e75e345d15065c8ca8
daniel-frank-miller <danielfrankmiller@gmail.com>
2024-09-27 14:38:58 +0330

    Initial

---
+++ Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using Riok.Mapperly.Abstractions;
+using ThePackage;
+
+var foo2 = new Foo2(1, 2);
+var foo1 = foo2.ToFoo1();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public record Bar1(
+	int X,
+	int Y
+);
+public record Bar2(
+	int X,
+	int Y
+);
+
+[Mapper]
+public static partial class MyMappers
+{
+	public static partial Bar2 ToBar2(this Bar1 bar1);
+}
---
+++ ThePackage/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace ThePackage;
+
+public class Class1
+{
+
+}
+
+public record Foo1(
+	int X,
+	int Y
+);
+public record Foo2(
+	int X,
+	int Y
+);
+
+[Mapper]
+public static partial class Mappers
+{
+	public static partial Foo1 ToFoo1(this Foo2 foo2);
+}
